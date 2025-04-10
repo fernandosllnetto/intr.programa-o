8e07eda1964afe09fd53df1be69086615cf4e676
fernandosllnetto <fernando.sllnetto@gmail.com>
2025-04-09 21:03:39 -0300

    Add files via upload

---
+++ exercicio14.py
@@ -0,0 +1,9 @@
+for num in range(1000, 2001):
+    if num % 11 == 5:
+        print(f'O número é {num}.')
+
+numero = 1000
+
+while numero <= 2000:
+    if numero % 11 == 5:
+        print(f'O número é {numero}.')
