ad7723340bb0b8e680ec4eeb45b02789f4c9cd33
fernandosllnetto <fernando.sllnetto@gmail.com>
2025-04-09 21:39:28 -0300

    Add files via upload

---
+++ desafio02.py
@@ -0,0 +1,8 @@
+nome_arquivo = 'arquivo.txt'
+
+numero= int(input('DIgitw o número desejado: '))
+with open(nome_arquivo, 'w', encoding='utf-8') as arquivo:
+    for m in range(1,11):
+        resultado = numero * m
+        arquivo.write(f'{numero} x {m} = {resultado}\n')
+print('--- Programa finalizado! ---')
